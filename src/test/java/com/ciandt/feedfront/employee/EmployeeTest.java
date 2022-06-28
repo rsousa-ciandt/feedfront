d55f52929a78be7b41e17fa8bccb1de04ebf3fa7
rsousa <rsousa@ciandt.com>
2022-06-28 10:42:06 -0300

    refatoração do teste "salvarEmployeeTest" conforme mudanças na classe Employee

--- src/test/java/com/ciandt/feedfront/employee/EmployeeTest.java
+++ src/test/java/com/ciandt/feedfront/employee/EmployeeTest.java
@@ -3,34 +3,43 @@ package com.ciandt.feedfront.employee;
 
 import com.ciandt.feedfront.excecoes.ArquivoException;
 import com.ciandt.feedfront.excecoes.ComprimentoInvalidoException;
+import com.ciandt.feedfront.excecoes.EmailInvalidoException;
+import com.ciandt.feedfront.excecoes.EmployeeNaoEncontradoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeeTest {
 
-    public Employee employeeTest;
-    public Employee empregado1;
+    public Employee employee1;
+    public Employee employee2;
     @BeforeEach
     public void initEach() throws ComprimentoInvalidoException {
-        employeeTest = new Employee("Jose", "Silveira", "j.silveira@email.com");
+        employee1 = new Employee("Jose", "Silveira", "j.silveira@email.com");
+        employee2 = null;
     }
 
     @Test
-    public void salvarEmployeeTest() throws ComprimentoInvalidoException, ArquivoException {
-        Employee empregado = new Employee("Jose", "Silveira", "j.silveira@email.com");
-        employeeTest.salvarEmployee(empregado);
-        Employee retornoDePesquisa = employeeTest.buscarEmployee(empregado.getId());
-        assertEquals(retornoDePesquisa.getId(), empregado.getId());
+    public void salvarEmployeeTest() throws ComprimentoInvalidoException, EmailInvalidoException, ArquivoException, EmployeeNaoEncontradoException {
+        employee2 = new Employee("João", "Silveira", "j.silveira@email.com");
 
-        Exception exception = assertThrows(ArquivoException.class, () ->
-                empregado1 = new Employee("Joao", "Silveira", "j.silveira@email.com")
-                );
+        Employee.salvarEmployee(employee1);
+        Employee retornoDePesquisa = Employee.buscarEmployee(employee1.getId());
+
+        assertEquals(retornoDePesquisa, employee1);
+
+        Exception emailException = assertThrows(EmailInvalidoException.class, () -> {
+           Employee.salvarEmployee(employee2);
+        });
+
         String mensagemEsperada = "E-mail ja cadastrado no repositorio";
-        String mensagemRecebida = exception.getMessage();
-        assertEquals(mensagemEsperada, mensagemRecebida);
+        String mensagemRecebida = emailException.getMessage();
 
+        assertEquals(mensagemEsperada, mensagemRecebida);
     }
 
     @Test
@@ -52,19 +61,18 @@ public class EmployeeTest {
     @Test
     public void nomeDeveTerComprimentoMaiorQueDois() {
         Exception exception = assertThrows(ComprimentoInvalidoException.class, () ->
-        employeeTest = new Employee("Ze", "Juvenil", "z.juvenil@ciandt.com")
+        employee1 = new Employee("Ze", "Juvenil", "z.juvenil@ciandt.com")
         );
 
         String mensagemEsperada = "Comprimento do nome deve ser maior que 2 caracteres.";
         String mensagemRecebida = exception.getMessage();
         assertEquals(mensagemEsperada, mensagemRecebida);
-
     }
 
     @Test
     public void sobrenomeDeveTerComprimentoMaiorQueDois() {
         Exception exception = assertThrows(ComprimentoInvalidoException.class, () ->
-        employeeTest = new Employee("Joao", "ao", "z.juvenil@ciandt.com")
+        employee1 = new Employee("Joao", "ao", "z.juvenil@ciandt.com")
         );
 
         String mensagemEsperada = "Comprimento do sobrenome deve ser maior que 2 caracteres.";
