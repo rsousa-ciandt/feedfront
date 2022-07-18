752d305676448be0a3164c5b82d6079d7c744aed
rsousa <rsousa@ciandt.com>
2022-07-18 08:18:38 -0300

    refatoração do EmployeeControllerTest para teste unitário com mockito

--- src/test/java/com/ciandt/feedfront/controllers/EmployeeControllerTest.java
+++ src/test/java/com/ciandt/feedfront/controllers/EmployeeControllerTest.java
@@ -1,22 +1,21 @@
 package com.ciandt.feedfront.controllers;
 
+import com.ciandt.feedfront.contracts.DAO;
+import com.ciandt.feedfront.contracts.Service;
 import com.ciandt.feedfront.controller.EmployeeController;
 import com.ciandt.feedfront.excecoes.ArquivoException;
 import com.ciandt.feedfront.excecoes.BusinessException;
 import com.ciandt.feedfront.excecoes.ComprimentoInvalidoException;
-import com.ciandt.feedfront.excecoes.EntidadeNaoEncontradaException;
 import com.ciandt.feedfront.models.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
-import java.util.UUID;
 
+import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -25,54 +24,65 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 // Ser capaz de levar o "pedido" ao "cozinheiro" e me trazer o "prato"
 public class EmployeeControllerTest {
     private Employee employee;
-    private EmployeeController controller = new EmployeeController();
+    private EmployeeController employeeController;
+    private Service<Employee> employeeService;
 
     @BeforeEach
-    public void initEach() {
-        try {
-            Files.walk(Paths.get("src/main/resources/data/employee/"))
-                    .filter(p -> p.toString().endsWith(".byte"))
-                    .forEach(p -> {
-                        new File(p.toString()).delete();
-                    });
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    @SuppressWarnings("unchecked")
+    public void initEach() throws ArquivoException, BusinessException {
+        employeeController = new EmployeeController();
+        employeeService = (Service<Employee>) Mockito.mock(Service.class);
+        employee = new Employee("João", "Silveira", "j.silveira@email.com");
 
-        try {
-            employee = new Employee("João", "Silveira", "j.silveira@email.com");
-            controller.salvar(employee); // Isso também pode ser considerado teste
-        } catch (BusinessException | ArquivoException | ComprimentoInvalidoException ignored) {
-        }
+        employeeController.setService(employeeService);
+
+        employeeController.salvar(employee);
     }
 
     @Test
     public void listar() {
-        Collection<Employee> employees = assertDoesNotThrow(controller::listar);
+        Collection<Employee> employees = assertDoesNotThrow(employeeController::listar);
 
         assertTrue(employees instanceof List);
     }
 
     @Test
-    public void buscar() {
+    public void buscar() throws IOException, BusinessException {
         String uuid = employee.getId();
-        Employee employeeSalvo = assertDoesNotThrow(() -> controller.buscar(uuid));
+        when(employeeService.buscar(uuid)).thenReturn(employee);
+
+        Employee employeeSalvo = assertDoesNotThrow(() -> employeeController.buscar(uuid));
 
         assertEquals(employee, employeeSalvo);
     }
 
     @Test
-    public void atualizar() {
+    public void salvar() throws IOException, BusinessException {
+        when(employeeService.salvar(employee)).thenReturn(employee);
+
+        Employee employeeSalvo = assertDoesNotThrow(() -> employeeController.salvar(employee));
+
+        assertEquals(employee, employeeSalvo);
+    }
+
+    @Test
+    public void atualizar() throws IOException, BusinessException {
+        String uuid = employee.getId();
         employee.setEmail("joao.silveira@email.com");
-        Employee employeeAtualizado = assertDoesNotThrow(() -> controller.atualizar(employee));
+
+        when(employeeService.buscar(uuid)).thenReturn(employee);
+        when(employeeService.atualizar(employee)).thenReturn(employee);
+
+        Employee employeeAtualizado = assertDoesNotThrow(() -> employeeController.atualizar(employee));
 
         assertEquals(employee, employeeAtualizado);
     }
 
     @Test
-    public void apagar() {
+    public void apagar() throws IOException, BusinessException {
         String uuid = employee.getId();
+        when(employeeService.buscar(uuid)).thenReturn(employee);
 
-        assertDoesNotThrow(() -> controller.apagar(uuid));
+        assertDoesNotThrow(() -> employeeController.apagar(uuid));
     }
 }
