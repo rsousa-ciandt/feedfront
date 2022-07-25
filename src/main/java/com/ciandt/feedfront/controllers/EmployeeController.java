dd8acf9e66e2b37027b2d180ea2a17077003b8a6
mlewartoski <mlewartoski@ciandt.com>
2022-07-25 15:46:41 -0300

    Remover throws de exceções removidas, Employee.

--- src/main/java/com/ciandt/feedfront/controllers/EmployeeController.java
+++ src/main/java/com/ciandt/feedfront/controllers/EmployeeController.java
@@ -2,7 +2,6 @@ package com.ciandt.feedfront.controllers;
 
 import com.ciandt.feedfront.contracts.Service;
 import com.ciandt.feedfront.models.Employee;
-import com.ciandt.feedfront.excecoes.ArquivoException;
 import com.ciandt.feedfront.excecoes.BusinessException;
 
 import java.util.List;
@@ -14,23 +13,23 @@ public class EmployeeController {
         throw new UnsupportedOperationException();
     }
 
-    public List<Employee> listar() throws ArquivoException {
+    public List<Employee> listar() {
         throw new UnsupportedOperationException();
     }
 
-    public Employee buscar(String id) throws BusinessException, ArquivoException {
+    public Employee buscar(String id) throws BusinessException {
         throw new UnsupportedOperationException();
     }
 
-    public Employee salvar(Employee employee) throws BusinessException, ArquivoException {
+    public Employee salvar(Employee employee) throws BusinessException {
         throw new UnsupportedOperationException();
     }
 
-    public Employee atualizar(Employee employee) throws BusinessException, ArquivoException {
+    public Employee atualizar(Employee employee) throws BusinessException {
         throw new UnsupportedOperationException();
     }
 
-    public void apagar(String id) throws BusinessException, ArquivoException {
+    public void apagar(String id) throws BusinessException {
         throw new UnsupportedOperationException();
     }
 
--- src/main/java/com/ciandt/feedfront/daos/EmployeeDAO.java
+++ src/main/java/com/ciandt/feedfront/daos/EmployeeDAO.java
@@ -1,10 +1,8 @@
 package com.ciandt.feedfront.daos;
 
 import com.ciandt.feedfront.contracts.DAO;
-import com.ciandt.feedfront.excecoes.EntidadeNaoSerializavelException;
 import com.ciandt.feedfront.models.Employee;
 
-import java.io.IOException;
 import java.util.List;
 
 public class EmployeeDAO implements DAO<Employee> {
@@ -16,22 +14,22 @@ public class EmployeeDAO implements DAO<Employee> {
     }
 
     @Override
-    public List<Employee> listar() throws IOException, EntidadeNaoSerializavelException {
+    public List<Employee> listar() {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Employee buscar(String id) throws IOException, EntidadeNaoSerializavelException{
+    public Employee buscar(String id) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Employee salvar(Employee employee) throws IOException, EntidadeNaoSerializavelException {
+    public Employee salvar(Employee employee) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public boolean apagar(String id) throws IOException, EntidadeNaoSerializavelException {
+    public boolean apagar(String id)  {
         throw new UnsupportedOperationException();
     }
 }
