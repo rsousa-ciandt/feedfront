8c28012c7dbbd38a153e982945797518d3e32487
rsousa <rsousa@ciandt.com>
2022-07-26 13:30:23 -0300

    classe DAOTest apagada

--- src/main/java/com/ciandt/feedfront/App.java
+++ src/main/java/com/ciandt/feedfront/App.java
@@ -12,31 +12,6 @@ public class App {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("feedfront");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        try {
-            Employee employee1 = new Employee("JoÃ£o", "Silveira", "j.silveira@email.com");
-            Employee employee2 = new Employee("JoÃ£o1", "Silveira", "j.silveira@email.com");
-            Employee employee3 = new Employee("JoÃ£o2", "Silveira", "j.silveira@email.com");
-
-            DAOTest daoTest = new DAOTest(entityManager);
-
-//            daoTest.salvar(employee1);
-//            daoTest.salvar(employee2);
-//            daoTest.salvar(employee3);
-
-            for (Employee e: daoTest.listar()) {
-                System.out.println(e);
-            }
-
-            Employee employee = daoTest.buscar(employee1.getId());
-
-            System.out.println(employee);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            entityManager.close();
-            entityManagerFactory.close();
-        }
-
 
         System.out.println("Connected");
     }
