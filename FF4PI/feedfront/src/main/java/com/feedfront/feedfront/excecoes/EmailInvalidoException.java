2a105c4d83bf95d602b705cd9e75c90db72e77d5
Alessandra-gr <alessandrar@ciandt.com>
2022-08-03 16:06:09 -0300

    Parte do Employee

---
+++ FF4PI/feedfront/src/main/java/com/feedfront/feedfront/excecoes/EmailInvalidoException.java
@@ -0,0 +1,7 @@
+package com.feedfront.feedfront.excecoes;
+
+public class EmailInvalidoException extends BusinessException {
+    public EmailInvalidoException(String message) {
+        super(message);
+    }
+}
---
+++ FF4PI/feedfront/src/main/java/com/feedfront/feedfront/repository/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package com.feedfront.feedfront.repository;
+
+//TODO: IMPLEMENTE A INTERFACE JPAREPOSITORY E MAPEIE A CLASSE PARA O SPRINGBOOT
+public interface EmployeeRepository  {}
