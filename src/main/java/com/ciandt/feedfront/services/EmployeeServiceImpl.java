713fc4b5b72ce3f652ad3502f8aabea425fbb409
rsousa <rsousa@ciandt.com>
2022-08-04 09:11:55 -0300

    employee service implementation alterada para implementar a interface employee service

--- src/main/java/com/ciandt/feedfront/services/EmployeeServiceImpl.java
+++ src/main/java/com/ciandt/feedfront/services/EmployeeServiceImpl.java
@@ -7,27 +7,32 @@ import com.ciandt.feedfront.repositories.EmployeeRepository;
 import java.util.List;
 
 //TODO: IMPLEMENTE AS CLASSES E MAPEIE A CLASSE PARA O SPRINGBOOT
-public class EmployeeServiceImpl {
+public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
+    @Override
+    public List<Employee> listar() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Employee buscar(long id) throws BusinessException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
     public Employee salvar(Employee employee) throws BusinessException {
         throw new UnsupportedOperationException();
     }
 
-    public List<Employee> listaremployee() throws BusinessException {
-        throw new UnsupportedOperationException();
-    }
-
-    public Employee buscarPorId(Long id) throws BusinessException {
-        throw new UnsupportedOperationException();
-    }
-
-    public void apagar(Long id) throws BusinessException {
-        throw new UnsupportedOperationException();
-    }
-    // Utilize como exemplo o método atualizar do bloco III
+    @Override
     public Employee atualizar(Employee employee) throws BusinessException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void apagar(long id) throws BusinessException {
         throw new UnsupportedOperationException();
     }
 }
